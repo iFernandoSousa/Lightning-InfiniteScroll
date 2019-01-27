898fcb953e4cb839ea923ab49f5ed4621b7eaaa9
Fernando Sousa <iFernandoSousa@users.noreply.github.com>
2019-01-27 13:36:26 -0200

    Create InfiniteScrollHelper.js

---
+++ InfiniteScrollHelper.js
@@ -0,0 +1,50 @@
+({
+    loadData : function(component, sizeLimit) {
+        /*Controller Method receive how many records need to bring*/
+        let action = component.get('c.loadMoreData');
+        
+        component.set('v.isLoadinig', true);
+        
+        action.setParams({'sizeLimit' : sizeLimit});
+        
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            
+            if (state === 'SUCCESS') { 
+                let jsonResult = response.getReturnValue();
+                let resultData = JSON.parse(jsonResult);
+                
+                component.set('v.hasMore', resultData.length > sizeLimit);
+                
+                if (resultData.length > sizeLimit)
+                    resultData.splice(resultData.length - 1, 1);
+                
+                component.set('v.records', resultData);
+                component.set('v.isLoadinig', false);
+            }
+        });
+        
+        $A.enqueueAction(action);
+    },
+    prepareLoadMore: function(component) {
+        let sizeLimit = component.get('v.sizeLimit');
+        sizeLimit += 50;
+        
+        component.set('v.sizeLimit', sizeLimit);
+        
+        this.searchData(component, sizeLimit); 
+    },
+    loadMoreData : function(component, parent, target) {
+        let hasMore = component.get('v.hasMore');
+        let isLoading = component.get('v.isLoadinig');
+        
+        if (!isLoading && hasMore) {
+            let maxToReload = parent.scrollHeight - target.currentTarget.clientHeight - 150;
+            let positionScroll = Math.abs(parent.getBoundingClientRect().top);
+            
+            if (positionScroll >= maxToReload) {
+                this.prepareLoadMore(component);
+            }
+        }
+    }
+})
